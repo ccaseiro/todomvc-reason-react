424ec4969e74534b2b93a091621b0fe91482672a
Claudio Caseiro <ccaseiro@gmail.com>
2019-01-24 02:46:25 +0000

    install cypress

---
+++ cypress/integration/spec.js
@@ -0,0 +1,15 @@
+describe("spec", () => {
+  beforeEach(() => {
+    cy.visit("build/index.html");
+  });
+
+  context("No todos", () => {});
+  context("New todo", () => {});
+  context("Mark all as complete", () => {});
+  context("Item", () => {});
+  context("Editing", () => {});
+  context("Counter", () => {});
+  context("Clear completed button", () => {});
+  context("Persistence", () => {});
+  context("Routing", () => {});
+});
