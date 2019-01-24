64570b7c0396319142bd08291e84fbf845e41abd
Claudio Caseiro <ccaseiro@gmail.com>
2019-01-24 15:37:19 +0000

    spec: New todo - focus on the todo input field

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -5,13 +5,18 @@ describe("spec", () => {
 
   context("No Todos", () => {
     it("should hide #main and #footer", () => {
-      cy.get(".header");
+      cy.get(".header").should("exist");
       cy.get(".main").should("not.exist");
       cy.get(".footer").should("not.exist");
     });
   });
 
-  context("New todo", () => {});
+  context("New todo", () => {
+    it("should focus on the todo input field", function() {
+      cy.focused().should("have.class", "new-todo");
+    });
+  });
+
   context("Mark all as complete", () => {});
   context("Item", () => {});
   context("Editing", () => {});
--- src/Header.re
+++ src/Header.re
@@ -3,5 +3,12 @@ let component = ReasonReact.statelessComponent("Header");
 let make = _children => {
   ...component,
   render: _self =>
-    <header className="header"> {ReasonReact.string("Header")} </header>,
+    <header className="header">
+      <h1> {ReasonReact.string("todos")} </h1>
+      <input
+        className="new-todo"
+        placeholder="What needs to be done?"
+        autoFocus=true
+      />
+    </header>,
 };
